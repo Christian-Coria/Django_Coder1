c6b4ef6a59d2b200c16f81ddf184a7f018fa0b6f
Christian-Coria <desbloqueoenlinea@gmail.com>
2022-06-28 23:41:33 -0300

    DjangoBlog

---
+++ mi_app/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse 
+from datetime import datetime
+from mi_app.models import Curso
+
+# Create your views here.
+
+
+def saludo(request):
+
+    fecha_hora_ahora = datetime.now()
+    return HttpResponse(f"Mi Servicio Tecnico {fecha_hora_ahora}")
+
+def saludar_a(request,nombre):
+    return HttpResponse(f"Hola {nombre.capitalize()}")
+
+def saludo_personalizado(request):
+    context = {}
+
+    if request.GET:
+        context["nombre"] = request.GET["nombre"]
+
+    return render(request, "mi_app/index.html", context)
+
+def listar_cursos(request):
+    context={}
+    context ["cursos"] = Curso.objects.all()
+
+    return render(request,"mi_app/lista_cursos.html",context)
